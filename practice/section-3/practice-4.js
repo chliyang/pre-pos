42aec8bba2be3ae4354bfd299b29983c258918bd
chliyang <chengli.yang@thoughtworks.com>
2022-04-08 00:39:10 +0800

    finish: practice-4 in section-3

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,45 @@
-'use strict';
+"use strict";
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  const summaryCollection = countSameElements(collectionA);
+  return summaryCollection.map((element) =>
+    objectB.value.includes(element.key)
+      ? { key: element.key, count: getCount(element.count) }
+      : element
+  );
+}
+
+function getCount(number) {
+  return number - parseInt(number / 3);
+}
+
+function countSameElements(collection) {
+  const { keyList, countList } = getLists();
+
+  const result = [];
+  keyList.map((element, index) => {
+    result.push({ key: element, count: countList[index] });
+  });
+  return result;
+
+  function getLists() {
+    const keyList = [];
+    const countList = [];
+    collection.forEach((element) => {
+      if (keyList.includes(element.charAt(0))) {
+        countList[keyList.indexOf(element.charAt(0))] += getPlusCount(element);
+      } else {
+        keyList.push(element.charAt(0));
+        countList.push(getPlusCount(element));
+      }
+    });
+    return { keyList, countList };
+  }
+}
+
+function getPlusCount(element) {
+  if (element.length === 1) {
+    return 1;
+  }
+  return Number(element.replace(/[^0-9]/gi, ""));
 }
